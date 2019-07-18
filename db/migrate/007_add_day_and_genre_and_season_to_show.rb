1b1f2e957becd9ceb8299f2afa37bea14ddb143f
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-18 23:41:58 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_and_genre_and_season_to_show.rb
+++ db/migrate/007_add_day_and_genre_and_season_to_show.rb
@@ -1,9 +1,9 @@
 class AddDayAndGenreAndSeasonToShow  < ActiveRecord::Migration[5.1]
   
   def change
-    add_column :characters, :day, :string
-    add_column :characters, :genre, :string
-    add_column :characters, :season, :string
+    add_column :shows, :day, :string
+    add_column :shows, :genre, :string
+    add_column :shows, :season, :string
   end
   
 end
